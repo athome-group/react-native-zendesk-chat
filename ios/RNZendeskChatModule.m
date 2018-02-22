7137926a2dcc13e5102399c92591e578f1a8477b
Xiaochen Chen <xchen@adneom.lu>
2018-02-22 15:56:23 +0100

    iOS: - add framework search path for ZDCChat - add and export endChat method - update configs for preChatForm

--- ios/RNZendeskChatModule.m
+++ ios/RNZendeskChatModule.m
@@ -39,12 +39,18 @@ RCT_EXPORT_METHOD(startChat:(NSDictionary *)options) {
       if (options[@"tags"]) {
         config.tags = options[@"tags"];
       }
-      config.preChatDataRequirements.name       = ZDCPreChatDataRequired;
-      config.preChatDataRequirements.email      = options[@"emailNotRequired"] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequired;
+      config.preChatDataRequirements.name       = ZDCPreChatDataRequiredEditable;
+      config.preChatDataRequirements.email      = options[@"emailNotRequired"] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequiredEditable;
       config.preChatDataRequirements.phone      = options[@"phoneNotRequired"] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequired;
       config.preChatDataRequirements.department = options[@"departmentNotRequired"] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequiredEditable;
       config.preChatDataRequirements.message    = options[@"messageNotRequired"] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequired;
     }];
+  });
+}
+
+RCT_EXPORT_METHOD(endChat) {
+  dispatch_sync(dispatch_get_main_queue(), ^{
+    [ZDCChat endChat];
   });
 }
 
